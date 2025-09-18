ae108b9857cc8fce5ee00551eb844c65451aa529
hassanalsa3aka <hassansayed2002@hotmail.com>
2025-09-18 20:58:05 +0300

    full backend application

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,7 +18,7 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/test')
       .expect(200)
       .expect('Hello World!');
   });
